80407975ffc0be31de1537cfe787973e9a45ec8f
yu1k <48705423+yu1k@users.noreply.github.com>
2020-10-22 10:57:36 +0900

    Create : sshserver-setup.sh

---
+++ ubuntu/sshserver-setup.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+sudo apt update && apt upgrade -y
+
+sudo apt autoremove
+
+# openssh-server, vimをインストール
+sudo apt install -y \
+vim \
+openssh-server
+
+# openssh-serverがインストールができたかを確認
+dpkg -l | grep ssh
+
+# 直前のコマンドの結果の確認。エンターキーを押すまで待機
+read -p "Hit enter: "
+
+# サーバが動作しているかを確認
+ps -ax | grep ssh
+
+# 直前のコマンドの結果の確認。エンターキーを押すまで待機
+read -p "Hit enter: "
+
+# ファイアウォール
+sudo ufw enable
+
+sudo ufw status
+
+sudo ufw allow [開放するポート番号]
+
+sudo reload
+
+sudo ufw status
+
+# sshdの設定
+# バックアップ
+sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.old
+
+sudo su -
+
+# SShで使用するポートなどの設定をする
+sudo vim /etc/ssh/sshd_config
+
+# 直前のコマンドの結果の確認。エンターキーを押すまで待機
+read -p "Hit enter: "
+
+# sshdの再起動
+service sshd restart
+
+# 処理が完了したことを表示
+echo "Processing... Done"
