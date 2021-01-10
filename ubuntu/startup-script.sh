55c0052c3f22e4b639b4fd25a6e3e18f36cfbf32
yuik <48705423+yu1k@users.noreply.github.com>
2021-01-10 15:38:29 +0900

    Update startup-script.sh

--- ubuntu/startup-script.sh
+++ ubuntu/startup-script.sh
@@ -1,1 +1,30 @@
+#!/bin/bash
 
+set -x
+
+export DEBIAN_FRONTEND=noninteractive
+
+# Set Firewall
+if [ -e "/etc/network/if-pre-up.d/iptables" ]; then
+    rm -f /etc/network/if-pre-up.d/iptables
+    rm -f /etc/iptables/iptables.rules
+fi
+
+ufw allow 22222
+ufw default deny
+ufw --force enable
+
+# Set up ssh authentication
+apt install curl -y
+mkdir .ssh
+curl https://github.com/yu1k.keys > .ssh/authorized_keys
+chmod 600 ~/.ssh/authorized_keys
+
+# Change ssh settings
+echo 'PasswordAuthentication no' >> /etc/ssh/sshd_config
+echo 'PubkeyAuthentication yes' >> /etc/ssh/sshd_config
+
+# SSHコンフィグを再起動
+/etc/init.d/ssh restart
+
+reboot
